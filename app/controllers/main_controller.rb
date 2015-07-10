3240737219790e32b12e90c924d26b72ae6c6169
전민영 <minyoung@jeonmin-yeongui-MacBook.local>
2015-07-10 13:59:49 +0900

    18s

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,22 @@
 class MainController < ApplicationController
+require 'browser'
+  def index
+
+    if (browser.ie? || browser.ie6?)
+  
+      redirect_to "http://sbs18s.com"
+
+    end
+
+  end
 
   def notice
+
+    if (browser.ie? || browser.ie6?)
+  
+      redirect_to "http://sbs18s.com"
+
+    end
 
     @notice = true
 
